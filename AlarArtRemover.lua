1389d2b22dd8dc49bd13e41b68a1b945d952223c
alar <alar@aspide.it>
2008-10-05 17:31:12 +0000

    Moved to ACE3 and AlarShared Fiexe settings storage

---
+++ AlarArtRemover.lua
@@ -0,0 +1,78 @@
+--[[
+Name: AlarArtRemover.lua
+Revision: $Revision: 65902 $
+Author: Alar of Daggerspine
+Website: http://wow.aspide.it
+Documentation:
+SVN:
+Description:
+Dependencies: Alarmod
+License: GPL v2.1
+--]]
+--DEFAULT_CHAT_FRAME:AddMessage(GetTime().. " AlarArtRemover",1,0,0)
+local function help(self)
+--===DOCBEGIN===
+    self:HF_Title("Blizzard art remover")
+    self:HF_Commands()
+    self:RelNotes(2,0,0,[[
+Upgrade: Now uses Ace3
+Fixed: Art status is now always remember betweeen section
+]])
+    self:RelNotes(1,0,5,[[
+Upgrade: WoW 2.4 
+]])
+    self:RelNotes(1,0,4,[[
+Upgrade: Embedded libraries upgrade
+Fixed: ...AlarLib-2.0.lua:4689: bad argument #1 to 'pairs' (table expected, got nil)
+]])
+    self:RelNotes(1,0,0,[[
+Feature: Updated to WoW 2.3
+]])
+--===DOCEND===
+end
+local Id="AAR"
+local Fullname="AlarArtRemover"
+setglobal(Id,AlarCreateAddon(Fullname,true))
+local aar=getglobal(Id)
+aar.ID=Id
+aar.DATABASE='db' ..Id
+function aar:OnInitialized()
+    L=AlarGetLocale()
+    help(self)
+    g=self:AddToggle("HIDEGRYPHON",false,L["Hide gryphon"],L["If checked, hides gryphon art"])
+    g.width='full'
+    g=self:AddToggle("HIDEMAINBAR",false,L["Hide main bar"],L["If checked, hides main bar art"])
+    g.width='full'
+    g=self:AddToggle("HIDELAGMETER",false,L["Hide lagmeter"],L["If checked, hides lagmeter art"])
+    g.width='full'
+--[[
+    g=self:AddToggle("HIDEPETBAR",false,L["Hide pet bar"],L["If checked, hides pet bar art"])
+    g.width='full'
+--]]
+    self.HIDEGRYPHON={tipo="form",
+        items={"MainMenuBarLeftEndCap","MainMenuBarRightEndCap"}
+        }
+    self.HIDELAGMETER={tipo="form",
+                    items={"MainMenuBarPerformanceBarFrame"}
+                    }
+    self.HIDEMAINBAR={tipo="form",
+                    items={"MainMenuBarTexture0","MainMenuBarTexture1","MainMenuBarTexture2","MainMenuBarTexture3"}
+                    }
+    self.HIDEPETBAR={tipo="texture",
+                    items={"SlidingActionBarTexture0","SlidingActionBarTexture1"}
+                    }
+end
+function aar:Apply(toggle,value)
+    local work=self[toggle]
+    if (not work) then return end
+    if (work.tipo == "form") then
+        for _,f in pairs(work.items) do
+            if (not value) then
+                getglobal(f):Show()
+            else 
+                getglobal(f):Hide()
+            end             
+        end
+    end 
+end
+AAR=aar
---
+++ localization-enUS.lua
@@ -0,0 +1,11 @@
+AMO:Locale("enUS",{
+["Hide gryphon"]=true,
+["Hide pet bar"]=true,
+["Hide lagmeter"]=true,
+["Hide main bar"]=true,
+["If checked, hides gryphon art"]=true,
+["If checked, hides main bar art"]=true,
+["If checked, hides lagmeter art"]=true,
+["If checked, hides pet bar art"]=true
+}
+)
