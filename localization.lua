6adf2f67314843d7e15b3ff5eb6003ad07adbfbe
alar <alar@aspide.it>
2010-11-06 09:29:53 +0000

    Enabled french and german translations

--- localization.lua
+++ localization.lua
@@ -1,6 +1,5 @@
-local __FILE__=tostring(debugstack(1,2,0):match("(.*):1:")) -- MUST BE LINE 1
-local me, ns = ...
-print("Loading",__FILE__," inside ",me)
+local me,ns=...
+local lang=GetLocale()
 local l=LibStub("AceLocale-3.0")
 local L=l:NewLocale(me,"enUS",true,true)
 --@localization(locale="enUS", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
@@ -8,31 +7,31 @@ L=l:NewLocale(me,"frFR",true,true)
 if (L) then
 --@localization(locale="frFR", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"deDE",true,true)
+L=l:NewLocale(me,"deDE")
 if (L) then
 --@localization(locale="deDE", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"koKR",true,true)
+L=l:NewLocale(me,"koKR")
 if (L) then
 --@localization(locale="koKR", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"esMX",true,true)
+L=l:NewLocale(me,"esMX")
 if (L) then
 --@localization(locale="esMX", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"ruRU",true,true)
+L=l:NewLocale(me,"ruRU")
 if (L) then
 --@localization(locale="ruRU", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"zhCN",true,true)
+L=l:NewLocale(me,"zhCN")
 if (L) then
 --@localization(locale="zhCN", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"esES",true,true)
+L=l:NewLocale(me,"esES")
 if (L) then
 --@localization(locale="esES", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
-L=l:NewLocale(me,"zhTW",true,true)
+L=l:NewLocale(me,"zhTW")
 if (L) then
 --@localization(locale="zhTW", format="lua_additive_table" , escape-non-ascii=true, same-key-is-true=true, handle-unlocalized="english" )@
 end
