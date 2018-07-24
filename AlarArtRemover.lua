4c297debba10b524720f495209551465ab73c234
Alar of Runetotem <alar@aspide.it>
2018-07-24 09:52:20 +0200

    Battle for Azeroth

--- AlarArtRemover.lua
+++ AlarArtRemover.lua
@@ -1,20 +1,14 @@
 local __FILE__=tostring(debugstack(1,2,0):match("(.*):1:")) -- Always check line number in regexp and file
-local me,ns=...
-local pp=print
-local addon=ns
-function addon:Start()
-	local items={
-		MainMenuBarLeftEndCap,
-		MainMenuBarRightEndCap,
-		MainMenuBarTexture0,
-		MainMenuBarTexture1,
-		MainMenuBarTexture2,
-		MainMenuBarTexture3
-	}
-	for _,f in pairs(items) do
-		if f then f:Hide() end
-	end
-	
+print("AlarArtRemover loaded")
+C_Timer.After(2,function()
+  print("Hiding things")
+  local items={
+    MainMenuBarArtFrameBackground,
+    MainMenuBarArtFrame.LeftEndCap,
+    MainMenuBarArtFrame.RightEndCap
+  }
+  for _,f in pairs(items) do
+    if f then f:Hide() end
+  end
 end
-addon:Start()
-_G.AAR=addon
+)
