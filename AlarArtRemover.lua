84359d71a877cf2d36451682435cab89418a57b4
Alar of Runetotem <alar@aspide.it>
2023-05-08 14:20:53 +0200

    fixes

--- AlarArtRemover.lua
+++ AlarArtRemover.lua
@@ -1,5 +1,24 @@
 local __FILE__=tostring(debugstack(1,2,0):match("(.*):1:")) -- Always check line number in regexp and file
+local me,ns=...
 print("AlarArtRemover loaded")
+if (GetAccountExpansionLevel()==0) then
+  local addon=ns
+  function addon:Start()
+   local items={
+     MainMenuBarLeftEndCap,
+     MainMenuBarRightEndCap,
+     MainMenuBarTexture0,
+     MainMenuBarTexture1,
+     MainMenuBarTexture2,
+     MainMenuBarTexture3
+   }
+   for _,f in pairs(items) do
+     if f then f:Hide() end
+   end
+
+  end
+  addon:Start()
+else
 C_Timer.After(2,function()
   local items={
     MainMenuBar,EndCaps,
@@ -9,3 +28,4 @@ C_Timer.After(2,function()
   end
 end
 )
+end
