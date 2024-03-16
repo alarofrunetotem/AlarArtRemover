ce7784bb54edb2f6e57141fcee159c286d2aae25
Alar of Runetotem <alar@aspide.it>
2024-03-16 16:34:43 +0100

    Fixed classic version

--- AlarArtRemover.lua
+++ AlarArtRemover.lua
@@ -1,14 +1,15 @@
 local __FILE__=tostring(debugstack(1,2,0):match("(.*):1:")) -- Always check line number in regexp and file
 local me,ns=...
-print("AlarArtRemover loaded")
+print("AlarArtRemover loaded",GetAccountExpansionLevel(),"yo")
 if (GetAccountExpansionLevel()==0) then
   local addon=ns
   function addon:Start()
    local items={
-     MainMenuBarEndCaps
+     MainMenuBarLeftEndCap,
+     MainMenuBarRightEndCap
    }
    for _,f in pairs(items) do
-     if f then f:Hide() end
+     if f then f:Hide() print('hiding', f:GetName()) end
    end
 
   end
